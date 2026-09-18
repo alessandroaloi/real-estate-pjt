b033cb39c773179c564d3d60b99ac91c01a00b38
alessandroaloi <alejjaloi@gmail.com>
2026-09-18 16:09:26 +0200

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.FIREBASE_CONFIG = {
-  apiKey:            "INCOLLA_QUI",
-  authDomain:        "INCOLLA_QUI.firebaseapp.com",
-  projectId:         "INCOLLA_QUI",
-  storageBucket:     "INCOLLA_QUI.appspot.com",
-  messagingSenderId: "INCOLLA_QUI",
-  appId:             "INCOLLA_QUI"
+  apiKey:            "AIzaSyBSaRs9_LNxbqdHCt1MvAvm58HQ5DEC0SQ",
+  authDomain:        "reale-estate-pjt.firebaseapp.com",
+  projectId:         "reale-estate-pjt",
+  storageBucket:     "reale-estate-pjt.firebasestorage.app",
+  messagingSenderId: "1040864996029",
+  appId:             "1:1040864996029:web:b4687a736cb4d416ace5d4"
 };
