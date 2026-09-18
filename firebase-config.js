72fee6154bbf79da6a09a7a667b65b60cdbd164e
alessandroaloi <alejjaloi@gmail.com>
2026-09-18 15:53:10 +0200

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey:            "INCOLLA_QUI",
+  authDomain:        "INCOLLA_QUI.firebaseapp.com",
+  projectId:         "INCOLLA_QUI",
+  storageBucket:     "INCOLLA_QUI.appspot.com",
+  messagingSenderId: "INCOLLA_QUI",
+  appId:             "INCOLLA_QUI"
+};
